c50a38e2204a0e5b7ac3827901f453a56ec139b3
Deepanshu Singh <deepanshus@neelafilm.com>
2026-07-23 14:29:45 +0530

    [ADD] utils to add common utility function

---
+++ Assets/InGame/Utils/Editor/TypeDropdownDrawer.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace TMKOC.Utils.Editor {
+    [CustomPropertyDrawer(typeof(TypeDropdownAttribute))]
+    public class TypeDropdownDrawer : PropertyDrawer 
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) 
+        {
+            // Enforce that this attribute is only applied to string variables
+            if (property.propertyType != SerializedPropertyType.String) 
+            {
+                EditorGUI.LabelField(position, label.text, "Use [TypeDropdown] with strings.");
+                return;
+            }
+
+            TypeDropdownAttribute typeDropdownAttribute = (TypeDropdownAttribute)attribute;
+            
+            // Fast lookup of all concrete classes implementing the interface
+            var typeCollection = TypeCache.GetTypesDerivedFrom(typeDropdownAttribute.BaseType)
+                .Where(t => !t.IsAbstract && !t.IsInterface)
+                .ToList();
+
+            List<string> typeNames = typeCollection.Select(t => t.Name).ToList();
+
+            if (typeNames.Count == 0) 
+            {
+                EditorGUI.LabelField(position, label.text, "No valid types found");
+                return;
+            }
+
+            // Match the currently saved string to the dropdown index
+            int currentIndex = typeNames.IndexOf(property.stringValue);
+            if (currentIndex < 0) currentIndex = 0; 
+
+            // Draw the dropdown
+            int newIndex = EditorGUI.Popup(position, label.text, currentIndex, typeNames.ToArray());
+
+            // Save the newly selected string back to the SerializedProperty
+            property.stringValue = typeNames[newIndex];
+        }
+    }
+}
---
+++ Assets/InGame/Utils/TypeDropdownAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+namespace TMKOC.Utils{
+    /// <summary>
+    /// Place this attribute on a string field to render a dropdown of classes implementing the specified base type.
+    /// </summary>
+    public class TypeDropdownAttribute : PropertyAttribute 
+    {
+        public Type BaseType { get; }
+
+        public TypeDropdownAttribute(Type baseType) 
+        {
+            BaseType = baseType;
+        }
+    }
+}
