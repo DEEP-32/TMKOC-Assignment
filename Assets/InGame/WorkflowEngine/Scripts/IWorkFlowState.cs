6da4cbf1e65ef2c6c04f242cf2d93125751dae8a
Deepanshu Singh <deepanshus@neelafilm.com>
2026-07-23 12:28:30 +0530

    [ADD] basic interfaces for workflow engine

---
+++ Assets/InGame/WorkflowEngine/Scripts/IWorkFlowState.cs
@@ -0,0 +1,18 @@
+﻿namespace WorkFlowEngine.Runtime {
+    public interface IWorkFlowState {
+        void OnEnter();
+        void OnExit();
+        void Execute();
+        
+        event System.Action OnSuccess;
+        event System.Action<string> OnFailure;
+    }
+
+    public interface IWorkFlowEngine {
+        
+    }
+    
+    public interface IWorkFlowStateFactory {
+        IWorkFlowState Create(string className);
+    }
+}
