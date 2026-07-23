80ab2a5ba313ca8ee1d77bd1dadd48f6fa7d3c23
Deepanshu Singh <deepanshus@neelafilm.com>
2026-07-23 14:53:33 +0530

    [ADD] a basic bootstrapper

---
+++ Assets/InGame/WorkflowEngine/Scripts/WorkflowBootstrapper.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using WorkFlowEngine.Runtime.Data;
+
+namespace WorkflowEngine.Runtime {
+    public class WorkflowBootstrapper : MonoBehaviour {
+        [SerializeField] WorkFlowDefinition workflowDefinition;
+        
+    }
+}
