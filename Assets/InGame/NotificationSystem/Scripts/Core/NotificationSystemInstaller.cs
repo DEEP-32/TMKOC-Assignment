10c74fab609294ab753d389d716eab4eb4e4c5f0
Deepanshu Singh <anshu3012ep@gmail.com>
2026-07-24 18:14:10 +0530

    [UPDATED] set up the scene for notification installer

---
+++ Assets/InGame/NotificationSystem/Scripts/Core/NotificationSystemInstaller.cs
@@ -0,0 +1,8 @@
+﻿using NotificationSystem.Runtime.Pipeline.Config;
+using UnityEngine;
+
+namespace NotificationSystem.Runtime.Core {
+    public class NotificationSystemInstaller : MonoBehaviour {
+        [SerializeField] NotificationPipelineConfig pipelineConfig;
+    }
+}
---
+++ Assets/InGame/NotificationSystem/Scripts/Pipeline/Config/NotificationPipelineConfig.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using NotificationSystem.Runtime.Core;
+using TMKOC.Utils;
+using UnityEngine;
+
+namespace NotificationSystem.Runtime.Pipeline.Config {
+
+    //config for single pipeline
+    [System.Serializable]
+    public class PipelineConfigEntry {
+        [TypeDropdown(typeof(INotificationPipeline))]
+        public string Type;
+        
+        [field : SerializeField] public  BaseNotificationValidator Validator { get; private set; }
+        [field : SerializeField] public  BaseNotificationDelivery Delivery { get; private set; }
+        [field : SerializeField] public  BaseNotificationFormatter Formatter { get; private set; }
+        
+    }
+    
+    [CreateAssetMenu(fileName = "PipelineConfig", menuName = "NotificationSystem/Config", order = 0)]
+    public class NotificationPipelineConfig : ScriptableObject {
+        [SerializeField] List<PipelineConfigEntry> pipelines;
+        
+        public IReadOnlyList<PipelineConfigEntry> Pipelines => pipelines;
+    }
+}
